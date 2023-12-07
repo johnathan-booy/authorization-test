77a11b846733cf6ff1ec16e860e4037653110ea3
Johnathan Booy <106690990+johnathan-booy@users.noreply.github.com>
2023-12-07 15:49:31 -0800

    Handle missing id in database calls

--- api/models/User.js
+++ api/models/User.js
@@ -6,16 +6,19 @@ class User {
   }
 
   static async findById(id) {
+    if (!id) return null
     const result = await knex("users").select("*").where({ id: id })
     return result[0] || null
   }
 
   static async findByGoogleId(googleId) {
+    if (!googleId) return null
     const result = await knex("users").select("*").where({ google_id: googleId })
     return result[0] || null
   }
 
   static async create(newUser) {
+    if (!newUser) return null
     const result = await knex("users").insert(newUser).returning("*")
     return result[0] || null
   }
